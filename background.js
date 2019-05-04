5be5ae7dc1efab04b114df852048a7bd74e69efc
Slim Wang <netbot@vip.qq.com>
2019-05-04 13:33:36 +0800

    add popup

--- background.js
+++ background.js
@@ -19,7 +19,7 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'developers.chrome.com' },
+                pageUrl: { hostEquals: 'developer.chrome.com' },
             })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
